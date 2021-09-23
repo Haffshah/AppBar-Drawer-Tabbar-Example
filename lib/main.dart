a68d6d3551c6edd11dc32f6d99122ba0b0357f07
Harsh M Shah <shahharsh7786@gmail.com>
2021-09-23 17:14:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MainHome extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ElevatedButton(
-              style: ElevatedButton.styleFrom(primary: Colors.orange, shape: ),
+              style: ElevatedButton.styleFrom(primary: Colors.orange ),
               onPressed: () {
                 Navigator.push(
                   context,
